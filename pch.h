5d4aba5cc242ce354fb7fa6face13300887134eb
Shoxlu <100439482+Shoxlu@users.noreply.github.com>
2023-07-04 09:45:06 +0200

    delete unused files

--- pch.h
+++ pch.h
@@ -4,8 +4,14 @@
 // Toutefois, les fichiers listés ici sont TOUS recompilés si l'un d'entre eux est mis à jour entre les builds.
 // N'ajoutez pas de fichiers fréquemment mis à jour ici, car cela annule les gains de performance.
 
+#define _USE_MATH_DEFINES 
+#define _CRT_SECURE_NO_DEPRECATE
+#define _CRT_RAND_S
+#include <math.h>
+
 #ifndef PCH_H
 #define PCH_H
+
 
 // ajouter les en-têtes à précompiler ici
 #include "framework.h"
